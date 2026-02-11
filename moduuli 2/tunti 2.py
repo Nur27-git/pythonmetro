0e68ec8f6ef3366939330e7918e5afd1fb117417
nur27-git <nuutti27@gmail.com>
2026-02-11 09:49:29 +0200

    tunti 2

---
+++ moduuli 2/tunti 2.py
@@ -0,0 +1,20 @@
+nimi=input("Anna nimesi:")
+print(f"Terve, {nimi}")
+
+import math
+
+n=float(input("Ympyrän säde:"))
+print(f"Ympyrän pintä ala on {3.14*(n*n)}")
+
+kanta=int(input("Kanta:"))
+korkeus=int(input("Korkeus:"))
+print(f"Pinta-ala ={kanta*korkeus}")
+print(f"Piiri on {2*kanta+2*korkeus}")
+
+n1=int(input("Anna luku"))
+n2=int(input("Anna luku"))
+n3=int(input("Anna luku"))
+print(f"Summa on {n1+n2+n3}")
+print(f"Tulo on {n1*n2*n3}")
+print(f"Keskiarvo on {(n1+n2+n3)/3}")
+
